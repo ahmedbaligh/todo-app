ee27d7a401f818e8f2999135eecb51535cfbdbfe
Ahmed Baligh <ahmedm11@hotmail.com>
2021-02-15 18:04:32 +0200

    style: Clean unnecessary code

--- src/components/TodoHeader.js
+++ src/components/TodoHeader.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ThemeSwitcher from './ThemeSwitcher';
-import './TodoHeader.css';
 
-const TodoHeader = ({ darkTheme, onThemeChange }) => {
-  return (
-    <header className="todo-header">
-      <h1 className="todo-header">Todo</h1>
-      <ThemeSwitcher darkTheme={darkTheme} onThemeChange={onThemeChange} />
-    </header>
-  );
-};
+const TodoHeader = ({ darkTheme, onThemeChange }) => (
+  <header className="todo-header">
+    <h1 className="todo-header">Todo</h1>
+    <ThemeSwitcher darkTheme={darkTheme} onThemeChange={onThemeChange} />
+  </header>
+);
 
 export default TodoHeader;
